a68f292245094dc9641c55ad563ae1239ab9af5b
abasiofon-sendan <abasiofon135@gmail.com>
2026-03-27 16:48:24 +0100

    add: define 'app' variable for Vercel compatibility in WSGI config

--- backend/medinexus/wsgi.py
+++ backend/medinexus/wsgi.py
@@ -14,3 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'medinexus.settings')
 
 application = get_wsgi_application()
+app = application  # Required by Vercel
