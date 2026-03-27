4e4429aef05975bc4a657b61636ef3743bbbc5dd
abasiofon-sendan <abasiofon135@gmail.com>
2026-03-27 15:53:20 +0100

    changed database to supabase

--- backend/medinexus/settings.py
+++ backend/medinexus/settings.py
@@ -1,9 +1,11 @@
 """
 Django settings for medinexus project.
 """
-
+import os
 from pathlib import Path
 from datetime import timedelta
+from dotenv import load_dotenv
+import dj_database_url
 # from decouple import os.environ.get
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -82,12 +84,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'medinexus.wsgi.application'
 
-# Database
+# # Database
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+DATABASE_URL = os.environ.get('DATABASE_URL')
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600)
 }
 
 # Custom User Model
